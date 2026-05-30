46e51cf7d24460f46c6ff4e8ca3b8130f94e1b3f
virbunny <virbunny@gmail.com>
2026-05-31 01:45:35 +0800

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,36 @@
+// 2026-05-31 12:18:00;來自厲害的Chatgpt V58
+const CACHE_NAME = 'mitu-music-album-pwa-v58';
+const APP_ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './mitu-music-album-icon-v58-192.png',
+  './mitu-music-album-icon-v58-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(
+      keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request).then((response) => {
+      const copy = response.clone();
+      caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+      return response;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
